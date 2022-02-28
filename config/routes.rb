02c959a7211c04039a2ab8e6c37e41927eeb0058
Rahul <rahul.k@simformsolutions.com>
2022-02-28 22:07:39 +0530

    Performed the crud operation on product and customer model and Use 'business' as namespace for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/edit'
+  get 'products/show'
+  get 'products/new'
+  get 'customers/index'
+  get 'customers/new'
+  get 'customers/edit'
+  resources :products
+  resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :buisness do 
+    resources :customers
+  end  
 end
